c149782bbb0ef5b801c85b409f8035218b6cece4
Vincent Moore <devdebonair@gmail.com>
2016-12-29 04:07:08 -0700

    parsed gfycat width and height for video media

--- fetchers/fetcher-gfycat.js
+++ fetchers/fetcher-gfycat.js
@@ -2,8 +2,8 @@ module.exports = function gfycat(request, options, listing) {
     let extractGfycatVideo = function(data) {
         return {
             type: "video",
-            width: data.width,
-            height: data.height,
+            width: parseInt(data.width),
+            height: parseInt(data.height),
             poster: data.mobilePosterUrl,
             gif: data.max2mbGif,
             url: data.mobileUrl,
