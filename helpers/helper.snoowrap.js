ec5e4da857ee9c6f9182f769743a0bca3d3e0cf8
Vincent Moore <devdebonair@gmail.com>
2017-02-26 17:15:26 -0500

    added better check for regex in snoowrap error extraction

--- helpers/helper.snoowrap.js
+++ helpers/helper.snoowrap.js
@@ -1,6 +1,8 @@
+const _ = require("lodash");
+
 exports.parseStatusCode = function(message) {
     let codes = message.match(/\d{3,3}/);
-    if(codes.length > 0) {
+    if(!_.isEmpty(codes)) {
         return codes[0];
     }
     return null;
