dce2ef25977e22cb1257f106a75ba82088d18e0d
Vincent Moore <devdebonair@gmail.com>
2017-03-01 11:15:37 -0500

    removed unneccessary variables in server file

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const Express = require("express");
-const mcache = require("memory-cache");
 const Routes = require("./routes");
 const PORT = process.env.PORT || 3000;
-const expirationTime = 60 * 4; // 60 seconds
 
 const router = new Routes();
 const app = Express();
