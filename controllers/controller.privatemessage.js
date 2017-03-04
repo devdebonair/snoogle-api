3a4ba62548947ed5de641e4e8d0ee453bac56a73
Vincent Moore <devdebonair@gmail.com>
2017-03-04 07:27:43 -0500

    added delete and fetch for private messaging

--- controllers/controller.privatemessage.js
+++ controllers/controller.privatemessage.js
@@ -5,7 +5,7 @@ module.exports = class PrivateMessage extends RedditController {
         super(options);
     }
 
-    compose(options) {
+    compose(options = {}) {
         return new Promise((resolve, reject) => {
             if(this._.isEmpty(options.to) || this._.isEmpty(options.subject) || this._.isEmpty(options.text)) {
                 return reject(new this.RedditError("InvalidArguments", "Must provide to, subject and text."));
@@ -14,6 +14,32 @@ module.exports = class PrivateMessage extends RedditController {
             this.snoo
             .composeMessage(options)
             .then(_ => resolve({ success: true }))
+            .catch(error => this.parseSnooError(error, reject));
+        });
+    }
+
+    delete(options = {}) {
+        return new Promise((resolve, reject) => {
+            if(this._.isEmpty(options.id)) {
+                return reject(new this.RedditError("InvalidArguments", "Must provide id of private message."));
+            }
+            this.snoo
+            .getMessage(options.id)
+            .deleteFromInbox()
+            .then(_ => resolve({ success: true }))
+            .catch(error => this.parseSnooError(error, reject));
+        });
+    }
+
+    fetch(options = {}) {
+        return new Promise((resolve, reject) => {
+            if(this._.isEmpty(options.id)) {
+                return reject(new this.RedditError("InvalidArguments", "Must provide id of private message."));
+            }
+            this.snoo
+            .getMessage(options.id)
+            .fetch()
+            .then(resolve)
             .catch(error => this.parseSnooError(error, reject));
         });
     }
