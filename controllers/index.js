db73b256c3b81706e448b195956430b5e69835e1
Vincent Moore <devdebonair@gmail.com>
2017-03-04 14:02:08 -0500

    added multireddit, comment, and PM to controller index

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,14 @@ const User = require("./controller.user");
 const Listing = require("./controller.listing");
 const Submission = require("./controller.submission");
 const Subreddit = require("./controller.subreddit");
+const PrivateMessage = require("./controller.privatemessage");
+const Multireddit = require("./controller.multireddit");
+const Comment = require("./controller.comment");
 
 exports.User = User;
 exports.Listing = Listing;
 exports.Submission = Submission;
 exports.Subreddit = Subreddit;
+exports.PrivateMessage = PrivateMessage;
+exports.Multireddit = Multireddit;
+exports.Comment = Comment;
