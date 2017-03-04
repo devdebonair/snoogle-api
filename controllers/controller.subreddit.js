cfcbb8f14436177f45e174c44a1501048741dc12
Vincent Moore <devdebonair@gmail.com>
2017-03-04 16:55:05 -0500

    removed listing class and moved logic into subreddit controller

--- controllers/controller.subreddit.js
+++ controllers/controller.subreddit.js
@@ -75,4 +75,47 @@ module.exports = class Subreddit extends RedditController {
         });
     }
 
+    getListing(options = {}) {
+        const self = this;
+        const defaults = {
+            omit: [],
+            subreddit: null,
+            after: null,
+            sort: "hot"
+        };
+        options = this._.assign(defaults, options);
+        return new Promise((resolve, reject) => {
+            let subreddit = this.snoo;
+            let sort = options.sort.toLowerCase();
+
+            switch (sort) {
+                case "new":
+                subreddit = subreddit.getNew(options.subreddit, options); break;
+                case "hot":
+                subreddit = subreddit.getHot(options.subreddit, options); break;
+                case "rising":
+                subreddit = subreddit.getRising(options.subreddit, options); break;
+                case "top":
+                subreddit = subreddit.getTop(options.subreddit, options); break;
+                case "controversial":
+                subreddit = subreddit.getControversial(options.subreddit, options); break;
+                default:
+                return reject(this.errors.invalid.sort);
+            }
+
+            function fetchMedia(listings) {
+                return self.fetcher.fetchAllMedia(listings.data).then(data => {
+                    listings.data = data;
+                    return listings;
+                });
+            }
+
+            subreddit
+            .then(data => { return this.formatListing(data, options.omit); })
+            .then(fetchMedia)
+            .then(resolve)
+            .catch(error => this.parseSnooError(error, reject));
+        });
+    }
+
 };
