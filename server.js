05f191c671594047af6c3ec45dfedbdff25c0660
Vincent Moore <devdebonair@gmail.com>
2017-03-05 20:29:50 -0500

    removed old routes from server file

--- server.js
+++ server.js
@@ -12,14 +12,4 @@ app.use(MethodOverride());
 
 routes.connect(app);
 
-
-// app.get("/submission/:id", router.getSubmission());
-// app.post("/submission/:id/upvote", router.upvoteSubmission());
-// app.post("/submission/:id/downvote", router.downvoteSubmission());
-// app.post("/submission/:id/save", router.saveSubmission());
-// app.post("/submission/:id/unsave", router.unsaveSubmission());
-// app.post("/submission/:id/unvote", router.unvoteSubmission());
-//
-
-
 app.listen(PORT, _ => {});
