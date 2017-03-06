8a97a7c0a3e6a0f485946150a29a347141c8d245
Vincent Moore <devdebonair@gmail.com>
2017-03-05 21:02:47 -0500

    added route for private messages

---
+++ routes/api/route.privatemessage.js
@@ -0,0 +1,58 @@
+const PrivateMessage = require("../../controllers").PrivateMessage;
+const account = require("../../config").accounts.test;
+
+module.exports = (router) => {
+    router.route("/message")
+        .post((req, res) => {
+            const pm = new PrivateMessage(account);
+            const options = {to: req.body.to, subject: req.body.subject, text: req.body.text};
+            pm.compose(options)
+            .then(data => {
+                return res.status(200).json(data);
+            })
+            .catch(error => {
+                return res.status(error.code)
+                .json({type: error.name, message: error.message, code: error.code});
+            });
+        });
+
+    router.route("/message/:id")
+        .get((req, res) => {
+            const pm = new PrivateMessage(account);
+            const options = {id: req.params.id};
+            pm.fetch(options)
+            .then(data => {
+                return res.status(200).json(data);
+            })
+            .catch(error => {
+                return res.status(error.code)
+                .json({type: error.name, message: error.message, code: error.code});
+            });
+        })
+        .delete((req, res) => {
+            const pm = new PrivateMessage(account);
+            const options = {id: req.params.id};
+            pm.delete(options)
+            .then(data => {
+                return res.status(200).json(data);
+            })
+            .catch(error => {
+                return res.status(error.code)
+                .json({type: error.name, message: error.message, code: error.code});
+            });
+        });
+
+    router.route("/message/:id/reply")
+        .post((req, res) => {
+            const pm = new PrivateMessage(account);
+            const options = {id: req.params.id, text: req.body.text};
+            pm.reply(options)
+            .then(data => {
+                return res.status(200).json(data);
+            })
+            .catch(error => {
+                return res.status(error.code)
+                .json({type: error.name, message: error.message, code: error.code});
+            });
+        });
+};
