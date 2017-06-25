a7b1a02cd7f933207288c098638c317805fbcc14
Vincent Moore <devdebonair@gmail.com>
2017-06-25 14:34:27 -0400

    added cache to listing helper for fetching media

--- helpers/helper.listing.js
+++ helpers/helper.listing.js
@@ -3,6 +3,7 @@ const remark = require("remark");
 const remarkSqueeze = require("remark-squeeze-paragraphs");
 const markdownParser = require("../markdown");
 const Miner = require("../miner");
+const Cache = require("../cache");
 
 exports.format = (listing) => {
     return new Promise((resolve, reject) => {
@@ -45,21 +46,40 @@ exports.fetchMedia = (post) => {
     return new Promise((resolve, reject) => {
         let miner = new Miner();
         post.hamlet_media = [];
-        
-        miner
-        .fetch(post.url)
-        .then(media => {
-            post.hamlet_media = media;
-            if(_.isEmpty(post.hamlet_media) && !_.isEmpty(post.preview)) {
-                let preview = exports.getPreview(post);
-                post.hamlet_media.push(preview);
-            }
-            resolve(post);
-        })
-        .catch(error => {
-            console.log(error);
-            return resolve(post);
-        });
+
+        let cacheKey = post.url;
+        Cache
+            .shared()
+            .getJSON({key: cacheKey})
+            .then((cache) => {
+                if(!_.isEmpty(cache)) {
+                    post = _.assign(post, cache);
+                    return resolve(post);
+                }
+                miner
+                    .fetch(post.url)
+                    .then(media => {
+                        post.hamlet_media = media;
+                        if(_.isEmpty(post.hamlet_media) && !_.isEmpty(post.preview)) {
+                            let preview = exports.getPreview(post);
+                            post.hamlet_media.push(preview);
+                        }
+                        resolve(post);
+                        return post;
+                    })
+                    .then((post) => {
+                        const options = {
+                            key: cacheKey,
+                            value: _.pick(post, ["hamlet_media"]),
+                            exp: (60 * 60 * 24 * 1) // 1 day
+                        };
+                        return Cache.shared().storeJSON(options);
+                    })
+            })
+            .catch(error => {
+                console.log(error);
+                return resolve(post);
+            });
     });
 };
 
