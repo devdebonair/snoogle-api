8a879f2119cce102b15b8b2cb5408ee65d15085a
Vincent Moore <devdebonair@gmail.com>
2017-06-29 11:51:15 -0400

    converted submission controller from promises to async/await

--- controllers/controller.submission.js
+++ controllers/controller.submission.js
@@ -8,171 +8,143 @@ module.exports = class Submission extends RedditController {
         super(options);
     }
 
-    fetch(options = {}) {
-        return new Promise((resolve, reject) => {
-            const self = this;
-            if(this._.isEmpty(options.id)) {
-                return reject(new this.RedditError("InvalidArguments", "Must provide id for submission.", 500));
-            }
-            
-            this.snoo
-            .getSubmission(options.id)
-            .fetch()
-            .then((post) => {
-                post.comments = flatten({replies: post.comments.toJSON()});
-                return post;
-            })
-            .then(formatPost)
-            .then(fetchMedia)
-            .then(resolve)
-            .catch(error => this.parseSnooError(error, reject));
-        });
+    async fetch(options = {}) {
+        if(this._.isEmpty(options.id)) {
+            throw new this.RedditError("InvalidArguments", "Must provide id for submission.", 500);
+        }
+        try {
+            let submission = await this.snoo.getSubmission(options.id).fetch();
+            submission.comments = flatten({replies: submission.comments.toJSON()});
+            let formattedPost = formatPost(submission);
+            let postWithMedia = await fetchMedia(formattedPost);
+            return postWithMedia;
+        } catch(e) {
+            throw e;
+        }
     }
 
-    getComments(options) {
-        return new Promise((resolve, reject) => {
-            if(this._.isEmpty(options.id)) {
-                return reject(new this.RedditError("InvalidArguments", "Must provide id for submission.", 500));
-            }
-            this.snoo
-            .getSubmission(options.id).comments
-            .then(comments => comments.toJSON())
-            .then(comments => {
-                return { replies: comments };
-            })
-            .then(flatten)
-            .then(resolve)
-            .catch(error => this.parseSnooError(error, reject));
-        });
+    async getComments(options) {
+        if(this._.isEmpty(options.id)) {
+            throw new this.RedditError("InvalidArguments", "Must provide id for submission.", 500);
+        }
+        try {
+            let comments = await this.snoo.getSubmission(options.id).comments;
+            let commentsParsed = comments.toJSON();
+            let replies = {replies: comments};
+            let flattenedComments = flatten(replies);
+            return flattenedComments;
+        } catch(e) {
+            throw e;
+        }
     }
 
-    upvote(options = {}) {
-        return new Promise((resolve, reject) => {
-            if(this._.isEmpty(options.id)) {
-                return reject(new this.RedditError("InvalidArguments", "Must provide id for submission.", 500));
-            }
-            this.snoo
-            .getSubmission(options.id)
-            .upvote()
-            .then(resolve)
-            .catch(error => this.parseSnooError(error, reject));
-        });
+    async upvote(options = {}) {
+        if(this._.isEmpty(options.id)) {
+            throw new this.RedditError("InvalidArguments", "Must provide id for submission.", 500);
+        }
+        try {
+            return await this.snoo.getSubmission(options.id).upvote();
+        } catch(e) {
+            throw e;
+        }
     }
 
-    downvote(options = {}) {
-        return new Promise((resolve, reject) => {
-            if(this._.isEmpty(options.id)) {
-                return reject(new this.RedditError("InvalidArguments", "Must provide id for submission.", 500));
-            }
-            this.snoo
-            .getSubmission(options.id)
-            .downvote()
-            .then(resolve)
-            .catch(error => this.parseSnooError(error, reject));
-        });
+    async downvote(options = {}) {
+        if(this._.isEmpty(options.id)) {
+            throw new this.RedditError("InvalidArguments", "Must provide id for submission.", 500);
+        }
+        try {
+            return await this.snoo.getSubmission(options.id).downvote();
+        } catch(e) {
+            throw e;
+        }
     }
 
-    save(options = {}) {
-        return new Promise((resolve, reject) => {
-            if(this._.isEmpty(options.id)) {
-                return reject(new this.RedditError("InvalidArguments", "Must provide id for submission.", 500));
-            }
-            this.snoo
-            .getSubmission(options.id)
-            .save()
-            .then(resolve)
-            .catch(error => this.parseSnooError(error, reject));
-        });
+    async save(options = {}) {
+        if(this._.isEmpty(options.id)) {
+            throw new this.RedditError("InvalidArguments", "Must provide id for submission.", 500);
+        }
+        try {
+            return await this.snoo.getSubmission(options.id).save();
+        } catch(e) {
+            throw e;
+        }
     }
 
-    unsave(options = {}) {
-        return new Promise((resolve, reject) => {
-            if(this._.isEmpty(options.id)) {
-                return reject(new this.RedditError("InvalidArguments", "Must provide id for submission.", 500));
-            }
-            this.snoo
-            .getSubmission(options.id)
-            .unsave()
-            .then(resolve)
-            .catch(error => this.parseSnooError(error, reject));
-        });
+    async unsave(options = {}) {
+        if(this._.isEmpty(options.id)) {
+            throw new this.RedditError("InvalidArguments", "Must provide id for submission.", 500);
+        }
+        try {
+            return await this.snoo.getSubmission(options.id).unsave();
+        } catch(e) {
+            throw e;
+        }
     }
 
-    unvote(options = {}) {
-        return new Promise((resolve, reject) => {
-            if(this._.isEmpty(options.id)) {
-                return reject(new this.RedditError("InvalidArguments", "Must provide id for submission.", 500));
-            }
-            this.snoo
-            .getSubmission(options.id)
-            .unvote()
-            .then(resolve)
-            .catch(error => this.parseSnooError(error, reject));
-        });
+    async unvote(options = {}) {
+        if(this._.isEmpty(options.id)) {
+            throw new this.RedditError("InvalidArguments", "Must provide id for submission.", 500);
+        }
+        try {
+            return await this.snoo.getSubmission(options.id).unvote();
+        } catch(e) {
+            throw e;
+        }
     }
 
-    reply(options = {}) {
-        return new Promise((resolve, reject) => {
-            if(this._.isEmpty(options.id) || this._.isEmpty(options.text)) {
-                return reject(new this.RedditError("InvalidArguments", "Must provide submission id and text to send.", 500));
-            }
-            this.snoo
-            .getSubmission(options.id)
-            .reply(options.text)
-            .then(resolve)
-            .catch(error => this.parseSnooError(error, reject));
-        });
+    async reply(options = {}) {
+        if(this._.isEmpty(options.id) || this._.isEmpty(options.text)) {
+            throw new this.RedditError("InvalidArguments", "Must provide submission id and text to send.", 500);
+        }
+        try {
+            return await this.snoo.getSubmission(options.id).reply(options.text);
+        } catch(e) {
+            throw e;
+        }
     }
 
-    edit(options = {}) {
-        return new Promise((resolve, reject) => {
-            if(this._.isEmpty(options.id) || this._.isEmpty(options.text)) {
-                return reject(new this.RedditError("InvalidArguments", "Must provide submission id and text to update.", 500));
-            }
-            this.snoo
-            .getSubmission(options.id)
-            .edit(options.text)
-            .then(resolve)
-            .catch(error => this.parseSnooError(error, reject));
-        });
+    async edit(options = {}) {
+        if(this._.isEmpty(options.id) || this._.isEmpty(options.text)) {
+            throw new this.RedditError("InvalidArguments", "Must provide submission id and text to update.", 500);
+        }
+        try {
+            return await this.snoo.getSubmission(options.id).edit(options.text);
+        } catch(e) {
+            throw e;
+        }
     }
 
-    delete(options = {}) {
-        return new Promise((resolve, reject) => {
-            if(this._.isEmpty(options.id)) {
-                return reject(new this.RedditError("InvalidArguments", "Must provide id for submission.", 500));
-            }
-            this.snoo
-            .getSubmission(options.id)
-            .delete()
-            .then(resolve)
-            .catch(error => this.parseSnooError(error, reject));
-        });
+    async delete(options = {}) {
+        if(this._.isEmpty(options.id)) {
+            throw new this.RedditError("InvalidArguments", "Must provide id for submission.", 500);
+        }
+        try {
+            return await this.snoo.getSubmission(options.id).delete();
+        } catch(e) {
+            throw e;
+        }
     }
 
-    hide(options = {}) {
-        return new Promise((resolve, reject) => {
-            if(this._.isEmpty(options.id)) {
-                return reject(new this.RedditError("InvalidArguments", "Must provide id for submission.", 500));
-            }
-            this.snoo
-            .getSubmission(options.id)
-            .hide()
-            .then(resolve)
-            .catch(error => this.parseSnooError(error, reject));
-        });
+    async hide(options = {}) {
+        if(this._.isEmpty(options.id)) {
+            throw new this.RedditError("InvalidArguments", "Must provide id for submission.", 500);
+        }
+        try {
+            return await this.snoo.getSubmission(options.id).hide();
+        } catch(e) {
+            throw e;
+        }
     }
 
-    unhide(options = {}) {
-        return new Promise((resolve, reject) => {
-            if(this._.isEmpty(options.id)) {
-                return reject(new this.RedditError("InvalidArguments", "Must provide id for submission.", 500));
-            }
-            this.snoo
-            .getSubmission(options.id)
-            .unhide()
-            .then(resolve)
-            .catch(error => this.parseSnooError(error, reject));
-        });
+    async unhide(options = {}) {
+        if(this._.isEmpty(options.id)) {
+            throw new this.RedditError("InvalidArguments", "Must provide id for submission.", 500);
+        }
+        try {
+            return await this.snoo.getSubmission(options.id).unhide();
+        } catch(e) {
+            throw e;
+        }
     }
 };
